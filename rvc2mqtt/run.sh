8e0d937fb2adfdcb7f826250c2983138778c0fc6
Stephen Ayers <sayer@ayersnet.com>
2024-12-13 18:20:01 -0700

    set for HA

--- rvc2mqtt/run.sh
+++ rvc2mqtt/run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Ensure CAN interface is up
+echo "ip link set can0 up "
 ip link set can0 up 2>/dev/null || true
 
 # Start healthcheck in the background
+echo "starting health check..."
 /coachproxy/rv-c/healthcheck.pl &
 HEALTHCHECK_PID=$!
 
-# Start rvc2mqtt in the foreground
-/coachproxy/rv-c/rvc2mqtt.pl
+# Start rvc2mqtt in the background
+echo "starting rvc2mqtt..."
+/coachproxy/rv-c/rvc2mqtt.pl &
 
 #Start mqtt_rvc_set in the foreground
+echo "starting mqtt)rvc_set..."
 /coachproxy/rv-c/mqtt_rvc_set.pl
 
 # Function to clean up processes
