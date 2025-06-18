3868baba4bf21edf8f33ad8f459f9e3e1de903f6
Stephen Ayers <sayer@ayersnet.com>
2025-06-17 23:02:48 -0600

    set undefine groups to 00

--- rvc2mqtt/mqtt2rvc.pl
+++ rvc2mqtt/mqtt2rvc.pl
@@ -433,10 +433,10 @@ sub process_mqtt_message {
       push(@bytes, "FF");
     }
     
-    # Set group (byte 1) to FF if it's missing in the JSON
+    # Set group (byte 1) to 00 if it's missing in the JSON
     if (!defined $json->{'group'}) {
-      print "  Group parameter missing, setting to FF (all groups)\n" if $debug;
-      $bytes[1] = "FF";
+      print "  Group parameter missing, setting to 00\n" if $debug;
+      $bytes[1] = "00";
     }
     
     # Set all bits in byte 5 to 0 (no interlock active)
